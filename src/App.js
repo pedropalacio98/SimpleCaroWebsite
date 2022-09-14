97a2af54836210739fbac13a56de37c6d8070f8c
pedropalacioestrada <pedro.palacio.estrada@cern.ch>
2022-09-14 20:50:38 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Caro Simple website has arrived
         </p>
+        <h1>
+          Prepare your ass
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
