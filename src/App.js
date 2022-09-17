61fa2793f8cb08da850d3672c1ca0da0df3f8e1d
Francesco Ivone <francesco.ivone@cern.ch>
2022-09-17 11:59:07 +0200

    Intruduced new components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import pedrito from "./pedro.jpg"
+import Welcome from './welcome.js'
+import tick from './ThisTime.js'
+
+const name = "Pedro Palacio ";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The Caro Simple website has arrived
-        </p>
-        <h1>
-          Prepare your ass
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-    </div>
+        <h3 style={{color: 'gray' }}>
+        CERN is raising funds for this poor guy
+        </h3>
+        </header>
+    <h1>
+    <Welcome name={name} />
+    < tick ></tick>
+    </h1>      
+    <img  src={pedrito}  alt="fireSpot"/> 
+          <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+          >
+          
+      </a>
+          
+        </div>
   );
 }
 
