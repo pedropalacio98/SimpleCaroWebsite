9dc41fcfff85ecba7e3fa7b3a1e32ddf85549a81
pedropalacioestrada <pedro.palacio.estrada@cern.ch>
2022-09-28 15:54:16 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import pedrito from "./pedro.jpg"
-import Welcome from './welcome.js'
-import tick from './ThisTime.js'
-
-const name = "Pedro Palacio ";
-
+import Dashboard from "./Components/Dashboard/Dashboard";
+import {Routes, Route, Link, useLocation} from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import {AnimatePresence} from "framer-motion";
+import AnimatedRoutes from "./Components/AnimatedRoutes/AnimatedRoutes";
 
 function App() {
-  return (
-    <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3 style={{color: 'gray' }}>
-        CERN is raising funds for this poor guy
-        </h3>
-        </header>
-    <h1>
-    <Welcome name={name} />
-    < tick ></tick>
-    </h1>      
-    <img  src={pedrito}  alt="fireSpot"/> 
-          <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-          >
-          
-      </a>
-          
+    const location = useLocation();
+    return (
+        <div>
+            <NavBar />
+            <AnimatedRoutes />
         </div>
-  );
+    );
 }
 
 export default App;
